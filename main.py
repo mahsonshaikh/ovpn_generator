a17bda08f200993e7b5e92e548a77013d51bfa8c
ahsonshaikh <ahson.shaikh@arpatech.com>
2024-06-11 20:41:39 +0500

    More more commit

--- main.py
+++ main.py
@@ -16,20 +16,36 @@ def test_data():
 
 
 
-@app.route('/testapp/events', methods=["POST"])
+@app.route('/slack/events', methods=["POST"])
 def slack_events():
     # Running a simple command, e.g., 'ls'
-    payload = json.loads(request.form["payload"])
-    username = payload['user']['username']
-    user_id = payload['user']['user_id']
-    
-    result = subprocess.run([f'google-authenticator --time-based --disallow-reuse --force --rate-limit=3 --rate-time=30 --window-size=3 -l "{username}@dc-test.net" -s /etc/openvpn/otp/mubasit.khan.google_authenticator'], capture_output=True, text=True)
-    convertImageToPng(username=username)
-    slack_controller.upload_file(username=username, user_id=user_id )
-    # Printing the output and the return code
-    print(result.stdout)
-    print("Return code:", result.returncode)
+    data = request.form
+
+    if data.get("command") == "/testapp":
+        trigger_id = data.get("trigger_id")
+        username = data.get("user_name")
+        user_id = data.get('user_id')
+
+        result = subprocess.run([f'google-authenticator --time-based --disallow-reuse --force --rate-limit=3 --rate-time=30 --window-size=3 -l "{username}@dc-test.net" -s /etc/openvpn/otp/mubasit.khan.google_authenticator'], capture_output=True, text=True)
+        convertImageToPng(username=username)
+        slack_controller.upload_file(username=username, user_id=user_id )
+        # Printing the output and the return code
+        print(result.stdout)
+        print("Return code:", result.returncode)
+        return "Command if found!"
+
+
+
+
+        # username_full = helper.format_username(username)
+        # result = open_modal(trigger_id, username_full)
+        return '', 200
     return "Command if found!"
+    # payload = json.loads(request.form["payload"])
+    # username = payload['user']['username']
+    # user_id = payload['user']['user_id']
+    
+    
 
 
 @app.route('/slack/interactivity', methods=['POST'])
