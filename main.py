dce2bc2cb53ce8bee260cb98f6f60ebeea5010d4
ahsonshaikh <ahson.shaikh@arpatech.com>
2024-06-11 20:43:44 +0500

    More more commit

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def slack_events():
         username = data.get("user_name")
         user_id = data.get('user_id')
 
-        result = subprocess.run([f'google-authenticator --time-based --disallow-reuse --force --rate-limit=3 --rate-time=30 --window-size=3 -l "{username}@dc-test.net" -s /etc/openvpn/otp/mubasit.khan.google_authenticator'], capture_output=True, text=True)
+        result = subprocess.run([f'google-authenticator --time-based --disallow-reuse --force --rate-limit=3 --rate-time=30 --window-size=3 -l "{username}@dc-test.net" -s /etc/openvpn/otp/{username}.google_authenticator'], capture_output=True, text=True)
         convertImageToPng(username=username)
         slack_controller.upload_file(username=username, user_id=user_id )
         # Printing the output and the return code
